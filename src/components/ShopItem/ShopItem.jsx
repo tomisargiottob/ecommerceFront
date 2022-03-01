3af966f87e5fde734fa5afacb750977496198188
tomisargiottob <tomisargiottob@hotmail.com>
2022-03-01 21:38:48 +0100

    feature(shopItem): create shop Item boilerplate

---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,12 @@
+import './ShopItem.css';
+import { Container } from 'react-bootstrap';
+
+function ShopItem () {
+	return (
+		<Container className="shop-item-container">
+			Esto es un item dentro del List Container
+		</Container>
+	)
+}
+
+export default ShopItem
