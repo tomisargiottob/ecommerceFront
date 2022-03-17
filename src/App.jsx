b9d3193ec03e21421599c513f57c1ce39e7eef8e
tomisargiottob <tomisargiottob@hotmail.com>
2022-03-17 19:17:50 +0100

    feature(app): add routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import { React } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Cart from './components/Cart/Cart';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-// import Login from './components/Login/Login';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Container className="App">
-      <Row>
-        <NavBar />
-      </Row>
-      {/* <Row>
-        <Login />
-      </Row> */}
-      <Row>
-        <ItemListContainer welcome="Welcome to Mashipa Store" />
-      </Row>
-    </Container>
+    <BrowserRouter>
+      <Container className="App">
+        <Row>
+          <NavBar />
+        </Row>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ItemListContainer welcome="Welcome to Mashipa Store" />} />
+          <Route path="/categoria/:category" element={<ItemListContainer welcome="Mashipa Store" />} />
+          <Route path="/carrito" element={<Cart />} />
+          <Route path="/details/:id" element={<ItemDetailContainer />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
