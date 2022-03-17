6a6b0cac7984d78c195e617e5b0bf1f5be4080da
tomisargiottob <tomisargiottob@hotmail.com>
2022-03-17 19:22:36 +0100

    enhancement(shopItem): improve visualization of shopItem

--- src/components/ShopItem/ShopItem.jsx
+++ src/components/ShopItem/ShopItem.jsx
@@ -1,32 +1,56 @@
-import { Container, Row } from 'react-bootstrap';
-import { React } from 'react';
+import {
+  Card,
+} from 'react-bootstrap';
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ShopItem.css';
-import ItemCount from '../ItemCount/ItemCount';
+// import ItemCount from '../ItemCount/ItemCount';
 
 function ShopItem({ product }) {
+  const [display, setDisplay] = useState(false);
+
+  const showInfo = () => {
+    // setTimeout(() => {
+    if (display) {
+      setDisplay(false);
+    } else {
+      setDisplay(true);
+    }
+  };
   return (
-    <Container className="shop-item-container">
-      <Row>
-        <h3>
-          { product.name }
-        </h3>
-      </Row>
-      <Row>
-        <img src={product.image} alt="" srcSet="" />
-      </Row>
-      <Row>
-        <ItemCount initial={product.initial} stock={product.stock} name={product.name} />
-      </Row>
-    </Container>
+    <Link to={`/details/${product.id}`}>
+      <Card className="shop-item-container" style={{ width: '18rem' }} onMouseEnter={showInfo} onMouseLeave={showInfo}>
+        <Card.Img variant="top" src={product.thumbnail} />
+        {display && (
+          <Card.Body className="fadeIn">
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>
+              {product.price}
+              $
+            </Card.Text>
+          </Card.Body>
+        )}
+        {!display && (
+          <Card.Body className="fadeIn">
+            <Card.Text>
+              {product.price}
+              $
+            </Card.Text>
+          </Card.Body>
+        )}
+      </Card>
+    </Link>
   );
 }
 ShopItem.propTypes = {
   product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    initial: PropTypes.number.isRequired,
+    initial: PropTypes.number,
     stock: PropTypes.number.isRequired,
-    image: PropTypes.string,
+    price: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string,
   }).isRequired,
 };
 
