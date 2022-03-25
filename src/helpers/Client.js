6408119a9ed31cb1ee4a10ae59a06e8a81b701d7
tomisargiottob <tomisargiottob@hotmail.com>
2022-03-25 21:09:41 +0100

    enhancement(client): change client file name

--- src/helpers/Client.js
+++ src/helpers/Client.js
@@ -1,36 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable class-methods-use-this */
 import { React } from 'react';
 import { Col } from 'react-bootstrap';
 import ShopItem from '../components/ShopItem/ShopItem';
 
 class Client {
-  getProducts() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([
-          {
-            id: '15bef59f-b857-4162-a22d-64c459fdefee', stock: 15, initial: 1, name: 'Buso', thumbnail: 'https://mycestampadoscolombia.com/wp-content/uploads/2021/06/buso-hongo-768x768.png',
-          },
-          {
-            id: '15bef59f-b857-4162-a22d-64c459fdefef', stock: 3, initial: 1, name: 'Remera',
-          },
-          {
-            id: '15bef59f-b857-4162-a22d-64c459fdefeg', stock: 1, initial: 1, name: 'Pantalon',
-          },
-          {
-            id: '15bef59f-b857-4162-a22d-64c459fdefeh', stock: 2, initial: 1, name: 'Campera',
-          },
-        ]);
-      }, 5000);
-    }).then((items) => {
-      // eslint-disable-next-line max-len
-      const listItems = items.map((product) => (<Col className="shop-item" xs={12} md={6} lg={4} xl={3} key={product.id}><ShopItem product={product} /></Col>));
-      return listItems;
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
-
   async getProductsHeroku(category) {
     let url = 'https://ecommercesargiotto.herokuapp.com/api/products';
     if (category) {
