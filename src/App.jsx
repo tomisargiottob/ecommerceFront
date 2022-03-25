4ce8d1376dfd46b67cb74198cf0791b38ea89c87
tomisargiottob <tomisargiottob@hotmail.com>
2022-03-26 00:27:43 +0100

    feature(cartContext): create cart context

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,26 @@ import Cart from './components/Cart/Cart';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Login from './components/Login/Login';
+import CartContextProvider from './helpers/CartContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Container className="App">
-        <Row>
-          <NavBar />
-        </Row>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ItemListContainer welcome="Welcome to Mashipa Store" />} />
-          <Route path="/categoria/:category" element={<ItemListContainer welcome="Mashipa Store" />} />
-          <Route path="/carrito" element={<Cart />} />
-          <Route path="/details/:id" element={<ItemDetailContainer />} />
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-      </Container>
+      <CartContextProvider>
+        <Container className="App">
+          <Row>
+            <NavBar />
+          </Row>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<ItemListContainer welcome="Welcome to Mashipa Store" />} />
+            <Route path="/categoria/:category" element={<ItemListContainer welcome="Mashipa Store" />} />
+            <Route path="/carrito" element={<Cart />} />
+            <Route path="/details/:id" element={<ItemDetailContainer />} />
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </Container>
+      </CartContextProvider>
     </BrowserRouter>
   );
 }
---
+++ src/helpers/CartContext.js
@@ -0,0 +1,75 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable react/jsx-filename-extension */
+import {
+  React,
+  createContext,
+  useState,
+  useContext,
+} from 'react';
+import PropTypes from 'prop-types';
+
+const Context = createContext([]);
+export const useCartContext = () => useContext(Context);
+
+function CartContext({ children }) {
+  const [cartList, setCartList] = useState([]);
+  const addToCart = (item, quantity) => {
+    const itemToAdd = { item, quantity };
+    let duplicated = false;
+    const newCartList = cartList.map((product) => {
+      if (product.item.id === item.id) {
+        duplicated = true;
+        return { item: product.item, quantity: product.quantity + quantity };
+      }
+      return product;
+    });
+    if (duplicated) {
+      setCartList(newCartList);
+    } else {
+      setCartList([...cartList, itemToAdd]);
+    }
+  };
+
+  const removeItem = (item) => {
+    const newCartList = cartList.filter((product) => {
+      if (product.item.id !== item.id) {
+        return false;
+      }
+      return true;
+    });
+    setCartList(newCartList);
+  };
+
+  const clearCart = () => {
+    setCartList([]);
+  };
+
+  const isInCart = (item) => {
+    const presence = cartList.some((product) => {
+      if (product.item.id !== item.id) {
+        return false;
+      }
+      return true;
+    });
+    return presence;
+  };
+
+  return (
+    <Context.Provider value={{
+      cartList,
+      addToCart,
+      removeItem,
+      clearCart,
+      isInCart,
+    }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
+
+CartContext.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CartContext;
