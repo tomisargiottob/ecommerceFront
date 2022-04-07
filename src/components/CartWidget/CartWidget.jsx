37506453cf60ee07ab4eb8a58a1f3d38655f87ce
tomisargiottob <tomisargiottob@hotmail.com>
2022-04-07 21:07:57 +0200

    enhancement(cartWidget): add number of items to widget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,18 +1,23 @@
 import { React } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartArrowDown } from '@fortawesome/fontawesome-free-solid';
-import { useCartContext } from '../../helpers/CartContext';
+import { useCartContext } from '../../context/CartContext';
 import './CartWidget.css';
 
 function CartWidget() {
   const { cartList } = useCartContext();
+  let ammountProducts = 0;
+  cartList.forEach((product) => {
+    ammountProducts += product.quantity;
+  });
+
   return (
     <span className="cart-widget">
       <FontAwesomeIcon icon={faCartArrowDown} />
       {cartList && cartList.length
         ? (
           <span className="cart-notification">
-            {`${cartList.length}`}
+            {ammountProducts}
           </span>
         )
         : ''}
